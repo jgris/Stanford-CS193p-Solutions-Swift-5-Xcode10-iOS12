be02511af42f64bb74ba5520e8377250fbcd64c4
J. Grishin <jgrismail@icloud.com>
2019-07-28 08:18:35 -0700

    Assignment II: Set Game (No Extra)

---
+++ Set/Set/UITheme.swift
@@ -0,0 +1,23 @@
+//
+//  UITheme.swift
+//  Set
+//
+//  Created by J. Grishin on 7/17/19.
+//  Copyright © 2019 J. Grishin. All rights reserved.
+//
+
+import UIKit
+
+struct Theme {
+    static let backgroundColor:         UIColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    
+    static let cardColor:               UIColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    static let cardColorSelected:       UIColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+    static let cardColorMatched:        UIColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
+    
+    static let buttonCornerRadius:      CGFloat = 12
+    static let cardButtonCornerRadius:  CGFloat = 12
+    
+    static let buttonColor:             UIColor = #colorLiteral(red: 0.6198394299, green: 0.5399842858, blue: 0, alpha: 1)
+    static let buttonColorDisabled:     UIColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+}
